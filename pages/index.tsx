ee1e7a761ed653b2617706644321489992b13be7
gytks4 <gytks4@naver.com>
2021-08-11 19:52:00 +0900

    feat: 홈화면 컴포넌트 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import styled from "@emotion/styled/types/base";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import Main from "../components/home/Main";
+import Options from "../components/home/Options";
+
 export default function Home() {
-  return <div>123</div>;
+  return (
+    <>
+      <Header />
+      <Options />
+      <Main />
+      <Footer />
+    </>
+  );
 }
