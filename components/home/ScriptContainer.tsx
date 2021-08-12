37097f5d14814e76c2e60cc8af8f8779b579e90e
gytks4 <gytks4@naver.com>
2021-08-12 16:08:22 +0900

    feat: ScriptContainer 스타일링 완료

--- components/home/ScriptContainer.tsx
+++ components/home/ScriptContainer.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { colors } from "../../lib/constants/colors";
 
 function ScriptContainer() {
-  return <Styled.Root>123</Styled.Root>;
+  return (
+    <Styled.Root>
+      <textarea placeholder="스크립트를 입력하면 글자 수, 단어 수, 리딩 타임을 측정합니다."></textarea>
+      <Styled.TimeResult>
+        <div>Speech Time</div>
+        <div>0 sec</div>
+      </Styled.TimeResult>
+    </Styled.Root>
+  );
 }
 
 export default ScriptContainer;
@@ -10,7 +19,42 @@ export default ScriptContainer;
 const Styled = {
   Root: styled.section`
     flex: 1;
-    background-color: white;
+
     height: 100%;
+
+    textarea {
+      outline: 0;
+      border: 0;
+      border-radius: 10px;
+      background-color: white;
+      padding: 20px;
+      width: 100%;
+      height: 90%;
+      resize: none;
+    }
+  `,
+  TimeResult: styled.div`
+    display: flex;
+    width: 100%;
+    height: 10%;
+    color: ${colors.sub_navy};
+    font-size: 20px;
+    font-weight: bold;
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 10px;
+    }
+
+    & > div {
+      background-color: ${colors.main_yellow};
+      width: 40%;
+    }
+    div:nth-of-type(2) {
+      background-color: white;
+      width: 60%;
+    }
   `,
 };
