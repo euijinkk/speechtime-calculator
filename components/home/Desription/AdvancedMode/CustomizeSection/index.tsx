219ef2c6a64b73c101fb4e6c93e879f621cdb02e
gytks4 <gytks4@naver.com>
2021-08-14 09:42:39 +0900

    feat: 20초 textarea 스타일링

--- components/home/Desription/AdvancedMode/CustomizeSection/index.tsx
+++ components/home/Desription/AdvancedMode/CustomizeSection/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { useRef } from "react";
+import { colors } from "../../../../../lib/constants/colors";
 
 function CustomizeSection() {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   return (
     <Styled.Root>
       <Styled.CustomizeText>
         ∙ 20초동안 읽은 스크립트를 입력해주세요. 전체 스크립트의 PT - Time이
         계산됩니다.
       </Styled.CustomizeText>
+      <textarea
+        placeholder="20초 동안 읽은 스크립트를 ‘정확히' 입력해주세요. "
+        ref={textareaRef}
+      />
     </Styled.Root>
   );
 }
@@ -15,8 +22,21 @@ function CustomizeSection() {
 export default CustomizeSection;
 
 const Styled = {
-  Root: styled.section``,
+  Root: styled.section`
+    textarea {
+      margin-bottom: 40px;
+      outline: 0;
+      border: 1px solid ${colors.gray_light};
+      border-radius: 10px;
+      padding: 12px 15px;
+      width: 448px;
+      height: 109px;
+      resize: none;
+      font-size: 14px;
+    }
+  `,
   CustomizeText: styled.div`
-    margin-bottom: 22px;
+    margin-bottom: 12px;
+    line-height: 2;
   `,
 };
