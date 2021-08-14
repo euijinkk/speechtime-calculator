cb98ee31d3db18b89f8f1e02e6423b0b7bff58ca
gytks4 <gytks4@naver.com>
2021-08-14 09:23:53 +0900

    refact: 타이머 함수에 useCallback을 사용하여 함수 동등성 구현

--- components/home/Desription/AdvancedMode/StopWatchSection/StopWatch.tsx
+++ components/home/Desription/AdvancedMode/StopWatchSection/StopWatch.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { colors } from "../../../../../lib/constants/colors";
 import addZero from "../../../../../lib/utils/addZero";
 import useInterval from "../../../../../hooks/useInterval";
+import { useCallback } from "react";
 
 function StopWatch() {
   //   let startTime = 0;
@@ -14,20 +15,24 @@ function StopWatch() {
   const [sec, setSec] = useState("00");
   const [millisec, setMillisec] = useState("00");
   const [isStart, setIsStart] = useState(false);
-  const intervalRef = useRef(null);
+  const intervalRef: { current: NodeJS.Timeout | null } = useRef(null);
   // setInterval 을 clearInterval 하기 위해서는, useRef를 사용해야 한다. 그냥 const/let변수로 지정하면 해결되지 않는다.
+  // 그 이유는 매번 렌더링될 때마다, 함수가 새롭게 정의되기 때문이다. useRef를 사용하면, 렌더링에 상관없이 값이 유지되도록 할 수 있다.
+  // 값이 변해도 리렌더링 하길 원하지 않는 변수를 useRef로 지정한다.
 
   const handleStart = () => {
     isStart ? stopTimer() : startTimer();
     setIsStart((prev) => !prev);
   };
 
-  function startTimer() {
-    let tempStartTime;
+  const startTimer = useCallback(() => {
+    let tempStartTime: number;
     if (startTime) {
       tempStartTime = Date.now() - endTime + startTime;
+      //   setStartTime(Date.now() - endTime + startTime);
     } else {
       tempStartTime = Date.now();
+      //   setStartTime(Date.now());
     }
     setStartTime(tempStartTime);
 
@@ -37,14 +42,18 @@ function StopWatch() {
       setSec(addZero(nowTime.getSeconds()));
       setMillisec(addZero(Math.floor(nowTime.getMilliseconds() / 10)));
     }, 1);
-  }
+  }, [startTime, endTime]);
 
-  function stopTimer() {
+  //   1. setState 함수가 비동기이기 때문에, nowTime 에 startTime 을 넣으면, 값이 들어가지 않는 문제 발생
+  //   2. 그래서 setStartTime에 들어갈 인자를 tempStartTime 변수로 지정하여 사용한다.
+  //   3. tempStartTime 값이 들어가고, 사용되는 곳의 scope가 다르기 때문에, 공통된 곳에서 let tempStartTime; 을 선언해준다.
+
+  const stopTimer = useCallback(() => {
     clearInterval(intervalRef.current);
     setEndTime(Date.now());
-  }
+  }, []);
 
-  const handleReset = () => {
+  const handleReset = useCallback(() => {
     console.log("reset");
     console.log(intervalRef.current);
     clearInterval(intervalRef.current);
@@ -53,7 +62,7 @@ function StopWatch() {
     setSec("00");
     setMillisec("00");
     setIsStart(false);
-  };
+  }, []);
 
   return (
     <>
