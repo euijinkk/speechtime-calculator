bfea9fe5588342aa304968ecc847d0537d5a3ac3
gytks4 <gytks4@naver.com>
2021-09-05 03:09:01 +0900

    feat: footer 마크업

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,7 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import styled from "@emotion/styled";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <Styled.Root>
+      <Styled.Top>
+        <div>Speech Time - Calculator</div>
+      </Styled.Top>
+      <Styled.Middle>
+        <div>Feedback</div>
+        <div>
+          <img src="/assets/icons/github.svg" alt="github" />
+          <img src="/assets/icons/github.svg" alt="youtube" />
+        </div>
+      </Styled.Middle>
+      <Styled.Bottom>
+        <div>speechtime-calculator@gmail.com</div>
+        <div>© 2021. speechtime-calculator. all rights reserved.</div>
+      </Styled.Bottom>
+    </Styled.Root>
+  );
 }
 
 export default Footer;
+
+const Styled = {
+  Root: styled.footer`
+    width: 100%;
+    font-size: 12px;
+  `,
+  Top: styled.div`
+    font-size: 14px;
+    font-weight: bold;
+  `,
+  Middle: styled.div`
+    display: flex;
+    justify-content: space-between;
+  `,
+  Bottom: styled.div`
+    display: flex;
+    justify-content: space-between;
+  `,
+};
