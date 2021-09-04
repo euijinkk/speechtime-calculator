4f3b2bdb13a84216296dedf7fe71722903bd1aa2
gytks4 <gytks4@naver.com>
2021-09-04 18:04:37 +0900

    chore: speed innerText를 반응형 고려하여 띄어쓰기 제거

--- components/home/Desription/BasicMode/ScrollBarContent/index.tsx
+++ components/home/Desription/BasicMode/ScrollBarContent/index.tsx
@@ -6,8 +6,10 @@ function ScrollBarContent() {
   return (
     <>
       <Styled.SpeedText>
-        ∙ 말하는 속도를 지정해주세요. 해당 속도에 맞춰 전체 스크립트의 <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;PT Time이 계산됩니다.
+        {/* ∙ 말하는 속도를 지정해주세요. 해당 속도에 맞춰 전체 스크립트의 <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;PT Time이 계산됩니다. */}
+        ∙ 말하는 속도를 지정해주세요. 해당 속도에 맞춰 전체 스크립트의 PT Time이
+        계산됩니다.
       </Styled.SpeedText>
       <ScrollBarContainer />
     </>
