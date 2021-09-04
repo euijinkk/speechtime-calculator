7efedd23ef4ed890df7b2ed6f01c65c43da65cdf
gytks4 <gytks4@naver.com>
2021-09-05 03:26:29 +0900

    feat: countResult 반응형 구현

--- components/home/ScriptContainer/CountResult.tsx
+++ components/home/ScriptContainer/CountResult.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { responsiveSize } from "../../../lib/constants/size";
 
 interface Props {
   wordsNum: number;
@@ -18,12 +19,12 @@ function CountResult({
   return (
     <Styled.Root>
       <div>
-        공백 포함 : {charactersNumWithBlank} 자 / {bytesNumWithBlank} bytes
+        공백 포함 : {charactersNumWithBlank}자/{bytesNumWithBlank}bytes
       </div>
       <div>
-        공백 미포함 : {charactersNum} 자 / {bytesNum} bytes
+        공백 미포함 : {charactersNum}자/{bytesNum}bytes
       </div>
-      <div>단어수 : {wordsNum} 개</div>
+      <div>단어수 : {wordsNum}개</div>
     </Styled.Root>
   );
 }
@@ -38,9 +39,22 @@ const Styled = {
     bottom: 13px;
     justify-content: flex-end;
     margin-right: 20px;
+    white-space: nowrap;
     font-size: 14px;
     & > div {
       margin-right: 20px;
+    }
+
+    @media (max-width: 1020px) and (min-width: 900px) {
+      white-space: normal;
+    }
+
+    @media ${responsiveSize.tablet} {
+      font-size: 12px;
+    }
+
+    @media ${responsiveSize.mobile} {
+      font-size: 10px;
     }
   `,
 };
