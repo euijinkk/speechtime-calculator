b6a1d868f643f133526cf7b17d4281e056a5cb05
gytks4 <gytks4@naver.com>
2021-09-27 16:08:39 +0900

    test: test

--- components/home/Desription/BasicMode/index.tsx
+++ components/home/Desription/BasicMode/index.tsx
@@ -13,7 +13,7 @@ function BasicMode() {
   const [isAccordianOpened, setIsAccordianOpened] = useRecoilState(
     isAccordianOpenedState
   );
-
+  // dd
   return (
     <Styled.Root>
       {isMobile && !isAccordianOpened ? (
